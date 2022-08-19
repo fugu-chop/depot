9a96fefcdb007f8e3ef5a0a004e89fece827fa0b
Dean Wan <deanwan14@gmail.com>
2022-08-19 11:53:26 +1000

    fix: json method call

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -29,8 +29,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
-        format.json { render :show,
-        status: :created, location: @line_item }
+        format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
