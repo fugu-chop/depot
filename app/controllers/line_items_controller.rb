f0c6f08a1187c23d6ae025530ea05208022c0134
Dean Wan <deanwan14@gmail.com>
2022-08-27 13:56:11 +1000

    feat: add functionality to aggregate cart items

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -24,9 +24,9 @@ class LineItemsController < ApplicationController
   # POST /line_items or /line_items.json
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
-
-    respond_to do |format|
+    @line_item = @cart.add_product(product)
+    
+    respond_to do |format| 
       if @line_item.save
         format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
         format.json { render :show, status: :created, location: @line_item }
