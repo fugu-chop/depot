a2a0c7f329f9f96310cfa89d5b2902881fc80592
Dean Wan <deanwan14@gmail.com>
2022-08-28 15:13:01 +1000

    test: add test for aggregated products

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -18,12 +18,15 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
   test "should create line_item" do
     assert_difference('LineItem.count') do
       post line_items_url, params: { product_id: products(:ruby).id }
+      post line_items_url, params: { product_id: products(:ruby).id }
     end
     
     follow_redirect!
 
     assert_select 'h2', 'Your Cart'
+    assert_select 'td', "2"
     assert_select 'td', "Programming Ruby 1.9"
+
   end
 
   test "should show line_item" do
