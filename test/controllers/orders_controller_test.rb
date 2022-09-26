a7b46562fbf4ef29b9ab3c55005d06d59eb42e0c
Dean Wan <deanwan14@gmail.com>
2022-09-26 16:03:40 +1000

    feat: allow checkout functionality in cart

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -5,12 +5,18 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
     @order = orders(:one)
   end
 
+  test "requires item in cart" do
+    get new_order_url
+    assert_redirected_to store_index_path assert_equal flash[:notice], 'Your cart is empty'
+  end
+
   test "should get index" do
     get orders_url
     assert_response :success
   end
 
   test "should get new" do
+    post line_items_url, params: { product_id: products(:ruby).id }
     get new_order_url
     assert_response :success
   end
