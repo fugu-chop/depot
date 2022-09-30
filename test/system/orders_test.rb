7065e80e5d993e889c7b3f9a24eaadfbdf38b83e
Dean Wan <deanwan14@gmail.com>
2022-09-30 19:32:55 +1000

    test: add notes on broken test

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -47,4 +47,20 @@ class OrdersTest < ApplicationSystemTestCase
 
     assert_text "Order was successfully destroyed"
   end
+
+  # The pay type field is not being picked up, despite
+  # appearing in the UI in the test snapshot
+  # test "check routing number" do
+  #   visit store_index_url
+  #   click_on 'Add to Cart', match: :first
+  #   click_on 'Checkout'
+
+  #   fill_in 'order_name', with: 'Dave Thomas'
+  #   fill_in 'order_address', with: '123 Main Street'
+  #   fill_in 'order_email', with: 'dave@example.com'
+  #   select 'Check', from: 'Pay type'
+
+  #   assert_no_selector "#order_routing_number"
+  #   assert_selector "#order_routing_number"
+  # end
 end
