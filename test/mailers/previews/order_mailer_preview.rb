78fc9bcdd9821695382db5d547d7649d2c5b7e70
Dean Wan <deanwan14@gmail.com>
2022-10-02 16:35:59 +1100

    test: update preview functionality

--- test/mailers/previews/order_mailer_preview.rb
+++ test/mailers/previews/order_mailer_preview.rb
@@ -1,14 +1,12 @@
 # Preview all emails at http://localhost:3000/rails/mailers/order_mailer
 class OrderMailerPreview < ActionMailer::Preview
-
   # Preview this email at http://localhost:3000/rails/mailers/order_mailer/received
   def received
-    OrderMailer.received
+    OrderMailer.received(Order.find(1))
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/order_mailer/shipped
   def shipped
-    OrderMailer.shipped
+    OrderMailer.shipped(Order.find(1))
   end
-
 end
